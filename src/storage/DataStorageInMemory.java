b285745200c2e9e76006cfbf9d371dd0d7be3e65
Izecson <weijia.xu.ixia@gmail.com>
2017-06-05 14:49:48 +0800

    Add DataStorageInMemory

---
+++ src/storage/DataStorageInMemory.java
@@ -0,0 +1,43 @@
+package storage;
+
+import java.util.LinkedList;
+
+import prototype.DatabaseException;
+import type.Type;
+
+public class DataStorageInMemory implements DataStorage {
+	int cursor;
+	String tableName;
+	LinkedList<LinkedList<Type>> values;
+	
+	public DataStorageInMemory(String name) {
+		cursor = -1;
+		tableName = name;
+		values = new LinkedList<LinkedList<Type>>();
+	}
+
+	@Override
+	public void open() {
+		cursor = -1;
+	}
+
+	@Override
+	public boolean next() {
+		cursor++;
+		return cursor < values.size();
+	}
+
+	@Override
+	public void close() {
+		cursor = -1;
+	}
+
+	@Override
+	public LinkedList<Type> getValueList() throws DatabaseException {
+		if (cursor >= 0 && cursor < values.size()) {
+			return values.get(cursor);
+		} else {
+			throw new DatabaseException("No more records.");
+		}
+	}
+}
